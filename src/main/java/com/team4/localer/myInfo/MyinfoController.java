15e866d24a40f8b9753b2d14a755e10aa58fb8a8
lys9765 <lys9765@naver.com>
2021-04-19 20:19:54 +0900

    04-19 commit

--- src/main/java/com/team4/localer/myInfo/MyinfoController.java
+++ src/main/java/com/team4/localer/myInfo/MyinfoController.java
@@ -1,10 +1,26 @@
 package com.team4.localer.myInfo;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyinfoController {
+	
+	@RequestMapping("/myInfoCheck")
+	public String myInfoCheck() {
+		return "/myInfo/myInfoCheck";
+	}
+	@RequestMapping("/pwdCheck")
+	public ModelAndView pwdCheck(String userpwd, HttpSession session) {
+		String userid = (String)session.getAttribute("logId");
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("myInfo/myInfo");
+		
+		return mav;
+	}
 	@RequestMapping("/info")
 	public String main() {
 		return "myInfo/main";
