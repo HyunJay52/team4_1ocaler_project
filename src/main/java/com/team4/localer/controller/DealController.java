d4214f03339667f2b78bb087fbb2c4199afa0125
HyunJay52 <hyunjungk52@gmail.com>
2021-04-22 20:45:04 +0900

    04/22 create service package

---
+++ src/main/java/com/team4/localer/controller/DealController.java
@@ -0,0 +1,42 @@
+package com.team4.localer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class DealController {
+	
+	//착한발견 (셀러)
+	@RequestMapping("/selBard")
+	public String selBard() {
+		return "deal/sellBoard";
+	}
+
+	//동네직구(회원)
+	@RequestMapping("/memberBoard")
+	public String memberBoard() {
+		return "deal/memberBoard";
+	}
+
+	@RequestMapping("/selWrite")
+	public String selWrite() {
+		return "deal/sellWrite";
+	}
+	
+	@RequestMapping("memberWrite")
+	public String memberWrite() {
+		 return "deal/memberWrite";
+	}
+	
+	@RequestMapping("memberView")
+	public String memberView() {
+		return "deal/memberView";
+	}
+
+	@RequestMapping("sellView")
+	public String selView() {
+		return "deal/sellView";
+	}
+
+	
+}
---
+++ src/main/java/com/team4/localer/controller/LikeController.java
@@ -0,0 +1,5 @@
+package com.team4.localer.controller;
+
+public class LikeController {
+
+}
