6e71d0ad0e152ed0a0796c6fc123cd5d5dafece1
nsroj <nsroj@DESKTOP-39CHS5H>
2021-05-17 23:21:22 +0900

    commit

--- src/main/java/com/team4/localer/service/SellerService.java
+++ src/main/java/com/team4/localer/service/SellerService.java
@@ -4,8 +4,10 @@ import com.team4.localer.vo.SellitemVO;
 
 public interface SellerService {
 	public int sellerInsert(SellitemVO itemVO); //셀러 게시글 등록
-	
-	
 	public int itemOptionInsert(String userid, String option_title, String option_content, int o_price); // 아이템 옵션 인설트
 
+	
+	//1개 글 선택
+	
+	
 }
