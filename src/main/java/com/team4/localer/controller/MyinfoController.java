8ff1e300b52d79bf19d8c5ec8cc8c5c4a9f41271
lys9765 <lys9765@naver.com>
2021-05-23 15:40:08 +0900

    qna 페이지 작업중

--- src/main/java/com/team4/localer/controller/MyinfoController.java
+++ src/main/java/com/team4/localer/controller/MyinfoController.java
@@ -28,6 +28,7 @@ import com.team4.localer.vo.MyinfoDealVO;
 import com.team4.localer.vo.MyinfoJoinUsVO;
 import com.team4.localer.vo.MyinfoPageVO;
 import com.team4.localer.vo.OrderVO;
+import com.team4.localer.vo.QnAVO;
 
 @Controller
 public class MyinfoController {
@@ -217,8 +218,8 @@ public class MyinfoController {
 		Map<String, Object> result = new HashMap<String, Object>();	
 		String searchId = (String)ses.getAttribute("logId");
 		vo.setUserid(searchId);
-		vo.setOnePageRecord(5);
-		vo.setOnePageSize(5);
+		vo.setOnePageRecord(10);
+		vo.setOnePageSize(10);
 		vo.setNowNum(Integer.parseInt(req.getParameter("nowNum")));
 		vo.setStartPage(vo.getNowNum(), vo.getOnePageSize());
 		System.out.println("kategorie="+vo.getKategorie());
@@ -312,7 +313,7 @@ public class MyinfoController {
 		Map<String, Object> result = new HashMap<String, Object>();	
 		
 		vo.setUserid((String)ses.getAttribute("logId"));
-		vo.setOnePageRecord(5);
+		vo.setOnePageRecord(10);
 		vo.setOnePageSize(5);
 		vo.setNowNum(Integer.parseInt(req.getParameter("nowNum")));
 		if(vo.getSearchWord() == null || vo.getSearchWord().equals("")) {
@@ -395,7 +396,7 @@ public class MyinfoController {
 		
 		
 		map.put("itemPVO", vo);
-		//map.put("IList", service.selectItemReviewList(vo));
+		map.put("IList", service.selectItemReviewList(vo));
 		return map;
 	}
 	
@@ -535,7 +536,7 @@ public class MyinfoController {
 		
 		vo.setSearchDate(vo.getSearchDate()+" 00:00:00");
 		vo.setSearchDate2(vo.getSearchDate2()+" 23:59:59");
-		
+		System.out.println("qnaKey="+vo.getSearchKey());
 
 		if(vo.getKategorie().equals("board")) {
 			vo.setDateContent("b_writedate");
@@ -564,6 +565,25 @@ public class MyinfoController {
 		return map;
 	}
 	
+	@ResponseBody
+	@RequestMapping("/setQnA")
+	public QnAVO setQnA(HttpSession ses, HttpServletRequest req) {
+		QnAVO vo =  new QnAVO();
+		String userid = (String)ses.getAttribute("logId");
+		int q_num = Integer.parseInt(req.getParameter("q_num"));
+		vo = service.setQnA(q_num, userid);
+		return vo;
+	}
+	
+	@ResponseBody
+	@RequestMapping("/QnAAnswerWrite")
+	public int QnAAnswerWrite(HttpSession ses, QnAVO vo) {
+		int result = 0;
+		
+		result = service.QnAAnswerWrite(vo);
+		
+		return result;
+	}
 	@RequestMapping("/myInfoSaleHistory")
 	public String myInfoSaleHistory() {
 		return "myInfo/myInfoSaleHistory";
